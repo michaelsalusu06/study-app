257d9c7ffea565b8cf9c91edcf9de04980526e45
nothing. <ddwi2504@gmail.com>
2026-05-18 23:46:15 +0700

    remove the hello world so now we're not gonna greet the world no more

--- _server/src/app.service.ts
+++ _server/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getStatus(): string {
+    return "Well i guess we have leak here, you supposed not to see this if you're not from our developer, pls report it if you find this";
   }
 }
